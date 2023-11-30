3c3fcda0c6b643c634464ca66901f09026b4ee1e
Izunajune <izuchukwuijeudo@gmail.com>
2023-11-30 17:27:39 +0100

    hot hot

---
+++ MultiplicationTable.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+public static void main(String... args){
+Scanner sc = new Scanner(System.in);
+
+System.out.print("Enter number between 1 and 10: ");
+int num = sc.nextInt();
+
+int count = 1;
+
+if(num < 1 || num > 10){
+System.out.println("Oga follow intstructions! check your entry and try again" );
+}
+
+else if{
+   while(count < 13){
+      System.out.println(num+ " x " +count+ " = " +num * count );
+          count = count +1; 
+}else{
+}
+   }
+}
