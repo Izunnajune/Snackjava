51840d7aa96c7a96fa727c13dcc0f68293b768e6
Izunajune <izuchukwuijeudo@gmail.com>
2023-12-05 13:08:43 +0100

    hothot

--- PhoneMenu.java
+++ PhoneMenu.java
@@ -74,7 +74,9 @@ if(num1 == 2){
      System.out.println("\t5. Templates");
      System.out.println("\t6. Smileys");
      System.out.println("\t7. Message settings");
-              
+     System.out.println("\t8. Info service");
+     System.out.println("\t9. Voice mailbox number");
+     System.out.println("\t10.Service command editor");         
 
        System.out.print("\nselect option: ");
           int message = sc.nextInt();
@@ -86,56 +88,52 @@ if(num1 == 2){
 	        }if(message == 3){
 	          System.out.println("\nOutbox");
 		}if(message == 4){
-	          System.out.println("\nPicture messages :) ");
+	          System.out.println("\nPicture messages ");
                 }if(message == 5){
 	          System.out.println("\nTemplates ");
 		}if(message == 6){
-	          System.out.println("\nSmileys :)");
+	          System.out.println("\nSmileys :) ");
 		}if(message == 7){
-	          System.out.println("Message settings");
+	          
+
+                  System.out.println("Message settings");
 		   System.out.println(" \t1. Set 1");              
                      System.out.println("\t\t1. Message centre number ");
                      System.out.println("\t\t2. Message sent as ");
                      System.out.println("\t\t3. Message validity ");
-                   System.out.println(" \t2. Common  ");
-                     System.out.println(" \t\t1. Delivery reports");
-                     System.out.println(" \t\t2. Reply via same centre");
-                     System.out.println(" \t\t3. Character support");
-                 System.out.println("8. Info service");
-                 System.out.println("9. Voice mailbox");
-                 System.out.println("10.Service command editor");
+                   
+                        System.out.print("\nselect option: ");
+                            int set = sc.nextInt();
+                                       if(set == 1){
+	                                  System.out.println("\nMessage centre number ");
+		                           }if(set == 2){
+	                                   System.out.println("\nMessage sent as");
+		                            }if(set == 3){
+                                             System.out.println("\nMessage validity");
+                                             System.out.println(" \t2. Common  ");
+                                             System.out.println(" \t\t1. Delivery reports");
+                                             System.out.println(" \t\t2. Reply via same centre");
+                                             System.out.println(" \t\t3. Character support"); 
+                                             }
+                                             
+                                            
+                 }if(message == 8){
+                 System.out.println("\n Info service");
+                 }if(message == 9){
+                 System.out.println("\n Voice mailbox");
+                 }if(message == 10){                 
+                 System.out.println("\n Service command editor");
+                 }
 
 
 }
-               
-
-
-
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
 
 if(num1 == 3){
-        System.out.println(" Chat ");
+        System.out.println(" \n Chat ");
 }
 
 if(num1 == 4){
-        System.out.println(" Call register ");
+        System.out.println("4. \nCall register ");
         System.out.println("\t1. Missed calls");
         System.out.println("\t2. Recievd calls");
         System.out.println("\t3. Dialed numbers");
@@ -144,10 +142,85 @@ if(num1 == 4){
 	System.out.println("\t6. Show call costs");
 	System.out.println("\t7. Call cost settings ");	
 	System.out.println("\t8. Prepaid credit");
+
+             System.out.print("\nselect option: ");
+                  int register = sc.nextInt();
+
+              if(register == 1){
+	        System.out.println("\n Missed calls ");
+	       } 
+                if(register == 2){
+	          System.out.println("\n Recieved calls");
+	        }if(register == 3){
+	          System.out.println("\nDialed numbers");
+		}if(register == 4){
+	          System.out.println("\nErase recent call lists ");
+                }if(register == 5){
+	          System.out.println("\n4. Show call duration ");
+                  System.out.println("\t1. Last call duration ");
+                  System.out.println("\t2. All calls' duration ");
+                  System.out.println("\t3. Recieved calls' duration ");
+                  System.out.println("\t4. Dialled calls' duration ");
+                  System.out.println("\t5. Clear timers ");
+                
+                      System.out.print("\nselect option: ");
+                         int duration = sc.nextInt();
+                     if(duration == 1){
+	        System.out.println("\n Last call duration ");
+	       } 
+                if(duration == 2){
+	          System.out.println("\n All calls' duration");
+	        }if(duration == 3){
+	          System.out.println("\nRecieved calls' duration ");
+		}if(register == 4){
+	          System.out.println("\nDialled calls' duration ");
+                }if(register == 5){                    
+                  System.out.println("\nClear timers ");
+                }
+
+            }if(register == 6){
+	          System.out.println("\n6. Show call costs ");
+                    System.out.println("\t1. Last call cost ");
+                    System.out.println("\t2. All calls' cost ");
+                    System.out.println("\t3. Clear counters ");
+                     
+                        System.out.print("\nselect option: ");
+                            int cost = sc.nextInt();
+                                       if(cost == 1){
+	                                  System.out.println("\nLast call cost ");
+		                           }if(cost == 2){
+	                                   System.out.println("\nAll calls' cost");
+		                            }if(cost == 3){
+                                             System.out.println("\nClear counters");
+                                            }
+
+          }if(register == 7){
+              System.out.println("\n7. Call cost settings ");
+              System.out.println("\t1. Call cost limit ");
+              System.out.println("\t2. Show costs in ");
+              
+              System.out.print("\nselect option: ");
+                            int reg = sc.nextInt();
+                                        if(reg == 1){
+	                                   System.out.println("\nCall cost limit ");
+		                           }if(reg == 2){
+	                                   System.out.println("\nShow costs in ");
+                                           } 
+
+
+          }if(register == 8 ){
+             System.out.println("\n Prepaid credit  ");
+             }
+
+
+         
 }
 
+
+
+
 if(num1 == 5){
-        System.out.println(" Tones ");
+        System.out.println("5. Tones ");
         System.out.println("\t1. Ringing tone");
         System.out.println("\t2. Ringing volume");
         System.out.println("\t3. Incoming call alerts");
@@ -165,20 +238,109 @@ if(num1 == 6){
         System.out.println("\t2. Phone setting");
         System.out.println("\t3. Security settings");
         System.out.println("\t4. Restore factory settings");
-}
+        
+           System.out.print("\nselect option: ");
+                            int callSet = sc.nextInt();
+                             if(callSet == 1){
+	                      System.out.println(" Call settings ");
+	                      System.out.println("\t1. Automatic redial");
+		              System.out.println("\t2. Speed dialing");
+                              System.out.println("\t3. Call waiting options ");
+		              System.out.println("\t4. Own number setting");
+                              System.out.println("\t5. Phone line in use");
+                              System.out.println("\t6. Automatic answer");
+                              
+                               
+                                System.out.print("\nselect option: ");
+                                     int callSet1 = sc.nextInt();
+                                       if(callSet1 == 1){
+	                                  System.out.println("\n Automatic redial ");
+		                           }if(callSet1 == 2){
+	                                   System.out.println("\n Speed dial");
+		                            }if(callSet1 == 3){
+                                             System.out.println("\n Call waiting options ");
+                                            }if(callSet1 == 4){
+                                             System.out.println("\n Own number setting ");
+                                              }if(callSet1 == 5){
+                                             System.out.println("\n Phone line in use ... ");
+                                               }if(callSet1 == 6){
+                                             System.out.println("\n Automatic answer ");
+                                                  }
+
+
+
+                       
+                       }if(callSet == 2){
+	                      System.out.println(" Phone settings ");
+	                      System.out.println("\t1. Language");
+		              System.out.println("\t2. Cell info display");
+                              System.out.println("\t3. Welcome note ");
+		              System.out.println("\t4. Network selection");
+                              System.out.println("\t5. Lights");
+                              System.out.println("\t6. Confirm SIM service actions");
+                                       
+                                    System.out.print("\nselect option: ");
+                                     int phoneSet = sc.nextInt();
+                                       
+                                         if(phoneSet == 1){
+	                                  System.out.println("\n Phone settings ");
+		                           }if(phoneSet == 2){
+	                                   System.out.println("\n Language");
+		                            }if(phoneSet == 3){
+                                             System.out.println("\n Welcome note ");
+                                            }if(phoneSet == 4){
+                                             System.out.println("\n Network selection ");
+                                              }if(phoneSet == 5){
+                                             System.out.println("\n Lights ");
+                                               }if(phoneSet == 6){
+                                             System.out.println("\n Confirm SIM service actions ");
+                                                  }
+
+                               }if(callSet == 3){
+	                          System.out.println(" Security settings ");
+	                          System.out.println("\t1. PIN code request");
+		                  System.out.println("\t2. Call barring service");
+                                  System.out.println("\t3. Fixed dialing ");
+		                  System.out.println("\t4. Closed user group");
+                                  System.out.println("\t5. Phone security");
+                                  System.out.println("\t6. Change access codes");
+                                       
+                                     System.out.print("\nselect option: ");
+                                     int security = sc.nextInt();
+                                       
+                                         if(security == 1){
+	                                  System.out.println("\n PIN code ");
+		                           }if(security == 2){
+	                                   System.out.println("\n Call barring service");
+		                            }if(security == 3){
+                                             System.out.println("\n Fixed dialing");
+                                            }if(security== 4){
+                                             System.out.println("\n Closed user group ");
+                                              }if(security == 5){
+                                             System.out.println("\n Phone security ");
+                                               }if(security == 6){
+                                             System.out.println("\n Change access codes ");
+                                                }
+                           }if(callSet == 4){
+	                      System.out.println(" \n Restore factory settings ");
+                              }
+
+              
+            
+         }
 
 
 if(num1 == 7){
-         System.out.println(" Call Divert ");
+         System.out.println(" \nCall Divert ");
 }
 if(num1 == 8){
-         System.out.println(" Games ");
+         System.out.println(" \nGames ");
 }
 if(num1 == 9){
-         System.out.println(" Calculator ");
+         System.out.println(" \nCalculator ");
 }
 if(num1 == 10){
-         System.out.println(" Reminders ");
+         System.out.println(" \nReminders ");
 }
 if(num1 == 11){
         System.out.println(" Clock ");
@@ -186,15 +348,33 @@ if(num1 == 11){
         System.out.println("\t2. Clock settings");
         System.out.println("\t3. Date settings");
         System.out.println("\t4. Stop watch");
-	System.out.println("\t4. Countdown timer");
-	System.out.println("\t4. Auto update of date and time");
+	System.out.println("\t5. Countdown timer");
+	System.out.println("\t6. Auto update of date and time");
+
+                           System.out.print("\nselect option: ");
+                                     int clock = sc.nextInt();
+                                       if(clock == 1){
+	                                  System.out.println("\n Alarm clock ");
+		                           }if(clock == 2){
+	                                   System.out.println("\n Clock settings");
+		                            }if(clock == 3){
+                                             System.out.println("\n Date setting ");
+                                            }if(clock == 4){
+                                             System.out.println("\n Stop watch ");
+                                              }if(clock == 5){
+                                             System.out.println("\n Countdown timer ");
+                                               }if(clock == 6){
+                                             System.out.println("\n Auto upadate of date and time ");
+                                                }
+
+               
 }
 
 if(num1 == 12){
-         System.out.println(" Profiles ");
+         System.out.println(" \nProfiles ");
 }
 if(num1 == 13){
-         System.out.println(" Sim services ");
+         System.out.println(" \nSim services ");
 }
 
 
