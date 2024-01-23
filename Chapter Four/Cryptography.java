dca69207b3f01559af52b90ed8455b31f4dbb381
Izunnajune <151368567+Izunnajune@users.noreply.github.com>
2024-01-23 21:43:45 +0100

    Add files via upload

--- Chapter Four/Cryptography.java
+++ Chapter Four/Cryptography.java
@@ -5,42 +5,27 @@ public class Cryptography{
     Scanner scanner = new Scanner(System.in);
 
      System.out.print("Enter a four digit number: ");
-      int pin = scanner.nextInt();
+      String pin = scanner.nextLine();
 
-          while(pin < 1000 || pin > 9999){
+          while(pin.length() != 4){
             System.out.print("Error! \nEnter a four digit number: ");
-                pin = scanner.nextInt();
+                pin = scanner.nextLine();
              }
       
-      int encrypt1 = 0;
-      int encrypt2 = 0;
-      int encrypt4 = 0;
-      int encrypt3 = 0;
-      
-     
-       
-          int digit4 = pin % 10;
-          encrypt4 = (digit4 + 7) % 10;
-          int digit3A = pin % 100;
-          int digit3B = digit3A / 10;
-          encrypt3 = (digit3B + 7) % 10;
-          int digit2A = pin / 100;
-          int digit2B = digit2A % 10;
-          encrypt2 = (digit2B + 7) % 10;
-          int digit1A = pin / 1000;
-          encrypt1 = (digit1A + 7) % 10;
-              
+          int firstNumber  = Integer.parseInt(String.valueOf(pin.charAt(0)));
+          int secondNumber  = Integer.parseInt(String.valueOf(pin.charAt(1)));
+          int thirdNumber  = Integer.parseInt(String.valueOf(pin.charAt(2)));
+          int fourthNumber  = Integer.parseInt(String.valueOf(pin.charAt(3)));
           
-
-           int temp1 = encrypt1;
-           int temp2 = encrypt2;
-           encrypt1 = encrypt3;
-           encrypt2 = encrypt4;
-           encrypt3 = temp1;
-           encrypt4 = temp2;
+           int digit1 = ((firstNumber + 7) % 10);
+           int digit2 = ((secondNumber + 7) % 10);
+           int digit3 = ((thirdNumber + 7) % 10);
+           int digit4 = ((fourthNumber + 7) % 10);
+           
+           
         
 
- System.out.printf("%d%d%d%d", encrypt1, encrypt2, encrypt3, encrypt4);
+ System.out.print(""+ digit3 + digit4 + digit1 + digit2);
 
 
 
